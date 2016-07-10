f91ba0656e004a8613a4b7f9ff70afe1cdae7c9a
Chris Lankford <christopherlankford@gmail.com>
2016-07-10 10:30:07 -0700

    Wired up basic components and added an image, need to introduce webpack to get image to load

--- public/app.js
+++ public/app.js
@@ -2,7 +2,18 @@ const ProjectSummaryComponent = React.createClass({
     render: function() {
         return (
             <div>
-                Hello World
+                <img src="images/project1.png"></img>
+                <ProjectSummaryDetails />
+            </div>
+        );
+    },
+});
+
+const ProjectSummaryDetails = React.createClass({
+    render: function() {
+        return (
+            <div>
+                ProjectSummaryDetails Component
             </div>
         );
     },
