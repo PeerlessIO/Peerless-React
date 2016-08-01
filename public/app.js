b524a595e71acc1b41409af014b3510841358e73
Chris Lankford <christopherlankford@gmail.com>
2016-07-31 17:51:06 -0700

    Refactored to get image to show and to revert webpack

--- public/app.js
+++ public/app.js
@@ -1,4 +1,23 @@
-import { ProjectSummaryComponent } from 'components/ProjectSummaryComponent';
+const ProjectSummaryComponent = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <img src="public/images/project1.png"></img>
+                <ProjectSummaryDetails />
+            </div>
+        );
+    },
+});
+
+const ProjectSummaryDetails = React.createClass({
+    render: function() {
+        return (
+            <div>
+                ProjectSummaryDetails Component
+            </div>
+        );
+    },
+});
 
 ReactDOM.render(
     <ProjectSummaryComponent />,
--- public/components/ProjectSummaryDetails.js
+++
@@ -1,9 +0,0 @@
-const ProjectSummaryDetails = React.createClass({
-    render: function() {
-        return (
-            <div>
-                ProjectSummaryDetails Component
-            </div>
-        );
-    },
-});
