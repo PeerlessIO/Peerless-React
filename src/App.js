74e7c83fe6eeef53117a65f7738dbf44f3fa7f40
Chris Lankford <christopherlankford@gmail.com>
2016-08-17 20:23:53 -0700

    Implementing basic navigation

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Router, Route, Link, hashHistory } from 'react-router';
+import './App.css';
+import Test from './Components/Test.js';
+
+const Home = () => <div><h1>Home</h1><Links/></div>;
+const About = () => <div><h1>About</h1><Links/></div>;
+const Contact = () => <div><h1>Contact</h1><Links/></div>;
+
+const Links = () =>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/contact">Contact</Link>
+  </nav>
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router history={ hashHistory }>
+          <Route path="/" component={Home}></Route>
+          <Route path="/about" component={About}></Route>
+          <Route path="/contact" component={Contact}></Route>
+        </Router>
+        <Test></Test>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Test = React.createClass({
+    render: function() {
+        return (
+            <div>
+                Test!!!
+            </div>
+        );
+    },
+});
+
+export default Test;
