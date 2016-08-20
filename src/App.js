1b04952d8a675e7788e166b4c46eae6f668f4552
Chris Lankford <christopherlankford@gmail.com>
2016-08-20 09:47:30 -0700

    Refactored to get Material-ui working properly

--- src/App.js
+++ src/App.js
@@ -1,53 +1,22 @@
 import React, { Component } from 'react';
-import { Router, Route, Link, hashHistory } from 'react-router';
 import './App.css';
-import Test from './Components/Test.js';
 
-// Part of Material-UI
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const Home = (props) => <div><h1>Home</h1><Links/>{props.children}</div>;
-const About = () => <div><h1>About</h1></div>;
-const Contact = () => <div><h1>Contact</h1></div>;
-
-const Links = () =>
-  <nav>
-    <Link activeStyle={{color: 'green'}} to="/">Home</Link>
-    <Link activeStyle={{color: 'green'}} to="/about">About</Link>
-    <Link activeStyle={{color: 'green'}} to="/contact">Contact</Link>
-  </nav>
+// Material-UI Components
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <Router history={ hashHistory }>
-          <Route path="/" component={Home}>
-            <Route path="about" component={About}></Route>
-            <Route path="contact" component={Contact}></Route>
-          </Route>
-        </Router>
-        <Test></Test>
-      </div>
+    return (    
+        <div>
+          <TextField
+            hintText="Username"
+          />
+          <TextField
+            hintText="Password"
+          />
+          <RaisedButton label="Sign In" />
+        </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// Part of Material-UI
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+// Material-UI Themes
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+// Import Components
+import App from './App.js';
+
+class Main extends Component {
+  render() {
+    return (
+        <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+            <App />
+        </MuiThemeProvider>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById("root"));
